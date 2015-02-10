55a16bef6f2404747ca73ec610c5c411f99e4de4
Jennifer <jnmurrin@gmail.com>
2015-02-09 21:22:35 -0800

    added bootstrap and some styles

--- index.php
+++ index.php
@@ -1,16 +1,29 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Message Board</title>
+
+    <!-- Bootstrap -->
+    <link href="bower_components/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+
     <body>
-        <?php
+
+        <div class="container">
+          
+
+          <?php
             require 'database.php';
             $pagesize = 4;
             if(isset($_GET['p'])){
@@ -24,7 +37,7 @@ and open the template in the editor.
             $results = $db->query($query);
 
             foreach($results as $gblist) :
-                echo $gblist['nickName'] . ' posted at: ' . $gblist['createtime'] . '<br>' .
+                echo '<h3>' . $gblist['nickName'] . ' <small>posted at: ' . $gblist['createtime'] . '</small></h3><br>' .
                     'contents: ' . $gblist['contents'] . '<br><hr>';
             endforeach;
             
@@ -45,8 +58,28 @@ and open the template in the editor.
                 }
             }
         ?>
+
+
+
+          <h3>Leave a Message</h3>
+          <form id="form1" name="form1" method="post" action="submitting.php">
+            <div class="form-group">
+              <label for="">Nick Name</label> (required, and less than 16 character)
+              <input class="form-control" id="nickname" name="nickname" type="text" required autofocus>
+            </div>
+            <div class="form-group">
+              <label for="">Email</label> (less than 60 characters)
+              <input class="form-control" id="email" name="email" type="email" required>
+            </div>
+            <div class="form-group">
+              <label for="">Message Contents</label>
+              <textarea class="form-control" id="content" name="content" rows="3" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+        </div>
         
-        <div class="form">
+        <!-- <div class="form">
             <form id="form1" name="form1" method="post" action="submitting.php">
                 <h3>Leave a Message</h3>
                 
@@ -68,6 +101,6 @@ and open the template in the editor.
                 </table>
                 <input type="submit" name="submit" value="Confirm" />
             </form>
-        </div>    
+        </div>     -->
     </body>
 </html>
